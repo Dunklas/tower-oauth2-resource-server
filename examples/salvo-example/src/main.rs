7abe8d91f806e8bf5b419d89a193a702310e6d7c
Rickard Andersson <dunklas@gmail.com>
2024-10-22 23:18:29 +0200

    Use tower-oauth2-resource-server in salvo example

--- examples/salvo-example/src/main.rs
+++ examples/salvo-example/src/main.rs
@@ -1,8 +1,30 @@
+use log::info;
 use salvo::prelude::*;
+use tower_oauth2_resource_server::server::OAuth2ResourceServer;
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().get(hello);
+    env_logger::init();
+    let oidc_provider = examples_util::start_oidc_provider().await;
+    let oidc_provider_host = oidc_provider.get_host().await.unwrap();
+    let oidc_provider_port = oidc_provider.get_host_port_ipv4(8080).await.unwrap();
+    info!(
+        "Running local OIDC provider on: {}:{}",
+        oidc_provider_host, oidc_provider_port
+    );
+
+    let oauth2_resource_server = <OAuth2ResourceServer>::builder()
+        .audiences(vec!["tors-example".to_owned()])
+        .issuer_uri(&format!(
+            "http://{}:{}/realms/tors",
+            oidc_provider_host, oidc_provider_port
+        ))
+        .build()
+        .expect("Failed to build OAuth2ResourceServer");
+
+    let router = Router::new()
+        .hoop(oauth2_resource_server.into_layer().compat())
+        .get(hello);
     let acceptor = TcpListener::new("127.0.0.1:3000").bind().await;
     Server::new(acceptor).serve(router).await;
 }

f99dca5934487394e3abb36bb0c04b4d227ada9b
Dunklas <dunklas@gmail.com>
2024-10-22 21:48:22 +0200

    Add tonic example (#4)
    Shows how to use tower-oauth2-resource-server in a gRPC server running
    with tonic.

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 Tower middleware that provides JWT authorization against an OpenID Connect (OIDC) Provider.
 
-Main inspiration for this middleware (both in naming and functionality) is [OAuth 2.0 Resource Server](https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/index.html).
+Main inspiration for this middleware (both in naming and functionality) is [Spring Security OAuth 2.0 Resource Server](https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/index.html).
 
 ## Example usage
 
 Check the [examples](./examples/).
 
-**Note!** To have an actual OIDC Provider in the examples, a local instance of [Keycloak](https://www.keycloak.org/) is started using docker.
-Each example will print what port the OIDC provider runs on to stdout.
+To run one of them: `RUST_LOG=info cargo run -p <name-of-example>`
 
-To obtain a valid jwt to use for the examples, you can run:
+**Note!** Since a running OIDC Provider is needed to use this middleware, a local instance of [Keycloak](https://www.keycloak.org/) is started in each example.
+It's seeded with a single user, *user@example.com* / *password*.
+
+Each example will log what port the OIDC provider runs on.
+
+To obtain a valid JWT for *user@example.com*, you can run:
+
 ```
 curl -X POST localhost:<PORT>/realms/tors/protocol/openid-connect/token \
     -H "Content-Type: application/x-www-form-urlencoded" \
---
+++ examples/tonic-example/src/main.rs
@@ -0,0 +1,76 @@
+use log::info;
+use tonic::{transport::Server, Request, Response, Status};
+use tower::ServiceBuilder;
+
+use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::{HelloReply, HelloRequest};
+use tower_oauth2_resource_server::claims::DefaultClaims;
+use tower_oauth2_resource_server::server::OAuth2ResourceServer;
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let oidc_provider = examples_util::start_oidc_provider().await;
+    let oidc_provider_host = oidc_provider.get_host().await.unwrap();
+    let oidc_provider_port = oidc_provider.get_host_port_ipv4(8080).await.unwrap();
+    info!(
+        "Running local OIDC provider on: {}:{}",
+        oidc_provider_host, oidc_provider_port
+    );
+
+    let oauth2_resource_server = <OAuth2ResourceServer>::builder()
+        .audiences(vec!["tors-example".to_owned()])
+        .issuer_uri(&format!(
+            "http://{}:{}/realms/tors",
+            oidc_provider_host, oidc_provider_port
+        ))
+        .build()
+        .expect("Failed to build OAuth2ResourceServer");
+
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    info!("Starting tonic on port 50051");
+    Server::builder()
+        .layer(
+            ServiceBuilder::new()
+                .layer(oauth2_resource_server.into_layer())
+                .into_inner(),
+        )
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        let claims = request
+            .extensions()
+            .get::<DefaultClaims>()
+            .ok_or(Status::internal("Failed to obtain JWT claims"))?;
+        let sub = claims
+            .sub
+            .as_ref()
+            .ok_or(Status::internal("Missing sub claim in JWT"))?;
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", sub),
+        };
+
+        Ok(Response::new(reply))
+    }
+}

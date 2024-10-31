16da61c58dac7d4230b14bf49721a9d33cc6902e
Rickard Andersson <dunklas@gmail.com>
2024-10-31 09:31:48 +0100

    raise threshold

--- tower-oauth2-resource-server/src/jwks.rs
+++ tower-oauth2-resource-server/src/jwks.rs
@@ -138,7 +138,7 @@ mod tests {
 
         let mut success = false;
         let start = Instant::now();
-        while start.elapsed() < Duration::from_millis(50) {
+        while start.elapsed() < Duration::from_millis(500) {
             if consumer.has_jwks().await {
                 success = true;
                 break;

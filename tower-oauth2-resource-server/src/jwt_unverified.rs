e09cb0f3174b3e346fdf566127392396937f8e1f
Rickard Andersson <dunklas@gmail.com>
2025-04-15 06:46:28 +0200

    fix clippy

--- tower-oauth2-resource-server/src/jwt_unverified.rs
+++ tower-oauth2-resource-server/src/jwt_unverified.rs
@@ -37,7 +37,7 @@ mod tests {
 
     use super::*;
 
-    const VALID_TOKEN: &'static str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
+    const VALID_TOKEN: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
 
     #[test]
     fn parse_header() {

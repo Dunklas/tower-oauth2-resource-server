190966b88dbe8bdd1d070d756ca75a02c5d1025f
Rickard Andersson <dunklas@gmail.com>
2025-09-03 07:00:01 +0200

    Rename handle_error to into_response

--- tower-oauth2-resource-server/src/error_handler.rs
+++ tower-oauth2-resource-server/src/error_handler.rs
@@ -3,7 +3,7 @@ use http::Response;
 use crate::error::AuthError;
 
 pub trait ErrorHandler<B>: Send + Sync {
-    fn handle_error(&self, error: &AuthError) -> Response<B>;
+    fn into_response(&self, error: &AuthError) -> Response<B>;
 }
 
 pub struct DefaultErrorHandler;
@@ -12,7 +12,7 @@ impl<B> ErrorHandler<B> for DefaultErrorHandler
 where
     B: Default,
 {
-    fn handle_error(&self, error: &AuthError) -> Response<B> {
+    fn into_response(&self, error: &AuthError) -> Response<B> {
         error.into()
     }
 }

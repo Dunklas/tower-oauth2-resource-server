4f82d1c5912a157a23ed4c2d8401623bbe64f8ad
Rickard Andersson <dunklas@gmail.com>
2025-09-20 10:50:02 +0200

    Make test context more usable :)

--- tower-oauth2-resource-server/tests/common/context.rs
+++ tower-oauth2-resource-server/tests/common/context.rs
@@ -1,21 +1,76 @@
-use tower_oauth2_resource_server::{server::OAuth2ResourceServer, tenant::TenantConfiguration};
-use wiremock::MockServer;
+use futures_util::future::join_all;
+use tower_oauth2_resource_server::{
+    server::OAuth2ResourceServer,
+    tenant::{TenantConfiguration, TenantConfigurationBuilder, TenantStaticConfigurationBuilder},
+};
+use wiremock::{
+    matchers::{method, path},
+    Mock, MockServer, ResponseTemplate,
+};
 
-use crate::common::mock_oidc_config;
+use crate::common::{mock_oidc_config, OpenIdConfig};
 
 pub const DEFAULT_ISSUER: &str = "https://auth-server.com";
 pub const DEFAULT_KID: &str = "default-kid";
 
 pub struct TestContext {
-    key_id: String,
-    audiences: Vec<String>,
-    tenant_configurations: Vec<TenantConfiguration>,
     mock_server: MockServer,
+    key_id: String,
+    tenant_configurations: Vec<TenantConfiguration>,
 }
 
 impl TestContext {
     pub fn builder() -> TestContextBuilder {
         TestContextBuilder::new()
+    }
+
+    pub async fn new(key_id: String, tenant_configurations: Vec<TenantInput>) -> Self {
+        let mock_server = MockServer::start().await;
+        for tenant_input in &tenant_configurations {
+            if let TenantInput::Oidc(issuer_path, _) = tenant_input {
+                Self::mock_oidc(&mock_server, issuer_path).await;
+            }
+        }
+        let tenants = join_all(
+            tenant_configurations
+                .iter()
+                .map(async |input| match input {
+                    TenantInput::Static(jwks, audiences) => {
+                        TenantConfiguration::static_builder(jwks)
+                            .audiences(&audiences)
+                            .build()
+                            .unwrap()
+                    }
+                    TenantInput::Oidc(issuer_path, audiences) => TenantConfiguration::builder(
+                        format!("{}{}", mock_server.uri(), issuer_path),
+                    )
+                    .audiences(&audiences)
+                    .build()
+                    .await
+                    .unwrap(),
+                })
+                .collect::<Vec<_>>(),
+        )
+        .await;
+        Self {
+            key_id,
+            tenant_configurations: tenants,
+            mock_server,
+        }
+    }
+
+    async fn mock_oidc(mock_server: &MockServer, issuer_path: &str) {
+        Mock::given(method("GET"))
+            .and(path(format!(
+                "{}/.well-known/openid-configuration",
+                issuer_path
+            )))
+            .respond_with(ResponseTemplate::new(200).set_body_json(OpenIdConfig {
+                issuer: format!("{}{}", &mock_server.uri(), issuer_path),
+                jwks_uri: format!("{}/jwks", &mock_server.uri()),
+            }))
+            .mount(mock_server)
+            .await;
     }
 
     pub async fn create_service(&self) -> OAuth2ResourceServer {
@@ -28,19 +83,15 @@ impl TestContext {
 }
 
 pub struct TestContextBuilder {
-    issuer: Option<String>,
     key_id: Option<String>,
-    audiences: Vec<String>,
-    tenant_configurations: Vec<TenantConfiguration>,
+    tenants: Vec<TenantInput>,
 }
 
 impl TestContextBuilder {
     pub fn new() -> Self {
         Self {
-            issuer: None,
             key_id: None,
-            audiences: Vec::new(),
-            tenant_configurations: Vec::new(),
+            tenants: vec![],
         }
     }
 
@@ -49,29 +100,17 @@ impl TestContextBuilder {
         self
     }
 
-    pub fn with_audience<S: Into<String>>(mut self, audience: S) -> Self {
-        self.audiences.push(audience.into());
-        self
-    }
-
-    pub fn with_tenant_configuration(mut self, config: TenantConfiguration) -> Self {
-        self.tenant_configurations.push(config);
+    pub fn with_tenant_configuration(mut self, config: TenantInput) -> Self {
+        self.tenants.push(config);
         self
     }
 
     pub async fn build(self) -> TestContext {
-        let mock_server = MockServer::start().await;
-        mock_oidc_config(
-            &mock_server,
-            &self.issuer.unwrap_or(DEFAULT_ISSUER.to_owned()),
+        TestContext::new(
+            self.key_id.unwrap_or_else(|| DEFAULT_KID.to_string()),
+            self.tenants,
         )
-        .await;
-        TestContext {
-            key_id: self.key_id.unwrap_or_else(|| DEFAULT_KID.to_string()),
-            audiences: self.audiences,
-            tenant_configurations: self.tenant_configurations,
-            mock_server,
-        }
+        .await
     }
 }
 
@@ -81,4 +120,7 @@ impl Default for TestContextBuilder {
     }
 }
 
-// Add utility functions for create_service, create_valid_jwt, etc.
+pub enum TenantInput {
+    Oidc(String, Vec<String>),
+    Static(String, Vec<String>),
+}

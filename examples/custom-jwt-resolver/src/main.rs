16878eec96b2e6853b65d8db19e353cd0903a638
Rickard Andersson <dunklas@gmail.com>
2025-11-06 10:22:26 +0100

    Add an example that shows how to provide a custom BearerTokenResolver

---
+++ examples/custom-jwt-resolver/src/main.rs
@@ -0,0 +1,94 @@
+use std::sync::Arc;
+
+use axum::{http::StatusCode, routing::get, Extension, Router};
+use log::info;
+use tokio::signal;
+use tower::ServiceBuilder;
+use tower_oauth2_resource_server::{
+    claims::DefaultClaims, error::AuthError, jwt_resolver::BearerTokenResolver,
+    jwt_unverified::UnverifiedJwt, server::OAuth2ResourceServer, tenant::TenantConfiguration,
+};
+
+struct CustomJwtResolver;
+
+impl BearerTokenResolver for CustomJwtResolver {
+    fn resolve(&self, request: &axum::http::Request<()>) -> Result<UnverifiedJwt, AuthError> {
+        Ok(UnverifiedJwt::new(
+            request
+                .headers()
+                .get("x-forwarded-access-token")
+                .ok_or(AuthError::MissingAuthorizationHeader)?
+                .to_str()
+                .map_err(|_| AuthError::InvalidAuthorizationHeader)?
+                .to_owned(),
+        ))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let oidc_provider = examples_util::start_oidc_provider().await;
+    let oidc_provider_host = oidc_provider.get_host().await.unwrap();
+    let oidc_provider_port = oidc_provider.get_host_port_ipv4(8080).await.unwrap();
+    info!("Running OIDC provider on port: {}", oidc_provider_port);
+
+    let oauth2_resource_server = <OAuth2ResourceServer>::builder()
+        .add_tenant(
+            TenantConfiguration::builder(format!(
+                "http://{}:{}/realms/tors",
+                oidc_provider_host, oidc_provider_port
+            ))
+            .audiences(&["tors-example"])
+            .build()
+            .await
+            .expect("Failed to build tenant configuration"),
+        )
+        .bearer_token_resolver(Arc::new(CustomJwtResolver))
+        .build()
+        .await
+        .expect("Failed to build OAuth2 resource server");
+
+    let app = Router::new()
+        .route("/", get(root))
+        .layer(ServiceBuilder::new().layer(oauth2_resource_server.into_layer()));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    info!("Running axum on port: 3000");
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn root(claims: Extension<DefaultClaims>) -> Result<(StatusCode, String), StatusCode> {
+    let sub = claims
+        .sub
+        .as_ref()
+        .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok((StatusCode::OK, format!("Hello, {}", sub)))
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
